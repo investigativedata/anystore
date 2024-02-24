e0eab7fb71cc60303904e36fdd46cbcf35507835
Simon Wörpel <simon.woerpel@pm.me>
2024-02-24 12:48:06 +0100

    ✅ (tests) add missing util test units

--- tests/test_util.py
+++ tests/test_util.py
@@ -34,3 +34,7 @@ def test_util_ensure_uri():
 
 def test_util_checksum():
     assert util.make_data_checksum("stable") == "a26dc899771c9e8503618745c4842c7d"
+    assert len(util.make_data_checksum("a")) == 32
+    assert len(util.make_data_checksum({"foo": "bar"})) == 32
+    assert len(util.make_data_checksum(True)) == 32
+    assert util.make_data_checksum(["a", 1]) != util.make_data_checksum(["a", "1"])
