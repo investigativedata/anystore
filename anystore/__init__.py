c85f1ee97b32b794f37c75fcfcec41cc40ea464c
Simon Wörpel <simon.woerpel@pm.me>
2024-07-26 13:42:32 +0200

    🚧 Add async decorator to global export

--- anystore/__init__.py
+++ anystore/__init__.py
@@ -1,11 +1,12 @@
 from anystore.store import get_store, Store
-from anystore.decorators import anycache
+from anystore.decorators import anycache, async_anycache
 from anystore.io import smart_read, smart_write, smart_stream
 
 __all__ = [
     "get_store",
     "Store",
     "anycache",
+    "async_anycache",
     "smart_read",
     "smart_write",
     "smart_stream",
