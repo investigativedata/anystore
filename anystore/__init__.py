b4c17babaa9ad0fa95129fc00595a00fb5398ade
Simon Wörpel <simon.woerpel@pm.me>
2024-09-30 10:23:50 +0200

    🚧 Tweak entrypoint default exports

--- anystore/__init__.py
+++ anystore/__init__.py
@@ -1,12 +1,12 @@
-from anystore.store import get_store, Store
 from anystore.decorators import anycache, async_anycache
-from anystore.io import smart_read, smart_write, smart_stream
+from anystore.io import smart_open, smart_read, smart_stream, smart_write
+from anystore.store import get_store
 
 __all__ = [
     "get_store",
-    "Store",
     "anycache",
     "async_anycache",
+    "smart_open",
     "smart_read",
     "smart_write",
     "smart_stream",
