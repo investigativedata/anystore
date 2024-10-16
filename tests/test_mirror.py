7026bda2cf9fc252b2db9f51da5a9ace30740bc3
Simon Wörpel <simon.woerpel@pm.me>
2024-10-16 08:38:43 +0200

    ✅ (tests/mirror) Test more

--- tests/test_mirror.py
+++ tests/test_mirror.py
@@ -1,8 +1,23 @@
+from moto import mock_aws
+
 from anystore.mirror import mirror
 from anystore.store import get_store
+from tests.conftest import setup_s3
 
 
+@mock_aws
 def test_mirror(tmp_path, fixtures_path):
+    KEYS = set(
+        [
+            "store.yml",
+            "store.json",
+            "model.yaml",
+            "model.json",
+            "lorem.txt",
+            "subdir/lorem.txt",
+        ]
+    )
+
     source = get_store(fixtures_path)
     target = get_store(tmp_path / "1")
     assert not len([k for k in target.iterate_keys()])
@@ -10,15 +25,17 @@ def test_mirror(tmp_path, fixtures_path):
     assert mirrored == 6
     assert skipped == 0
     assert source.get("lorem.txt") == target.get("lorem.txt")
-    assert len([k for k in target.iterate_keys()]) == len(
-        [k for k in source.iterate_keys()]
-    )
+    assert set(target.iterate_keys()) == KEYS
+    assert set(source.iterate_keys()) == KEYS
+
     mirrored, skipped = mirror(source, target)
     assert mirrored == 0
     assert skipped == 6
     mirrored, skipped = mirror(source, target, overwrite=True)
     assert mirrored == 6
     assert skipped == 0
+    assert set(target.iterate_keys()) == KEYS
+    assert set(source.iterate_keys()) == KEYS
 
     target = get_store(tmp_path / "2")
     mirrored, skipped = mirror(source, target, prefix="subdir")
@@ -26,3 +43,28 @@ def test_mirror(tmp_path, fixtures_path):
     assert skipped == 0
     assert len([k for k in target.iterate_keys()]) == 1
     assert source.get("lorem.txt") == target.get("subdir/lorem.txt")
+
+    setup_s3()
+    target = get_store("s3://anystore/test-mirror")
+    mirrored, skipped = mirror(source, target)
+    assert mirrored == 6
+    assert skipped == 0
+    assert source.get("lorem.txt") == target.get("lorem.txt")
+    assert set(target.iterate_keys()) == KEYS
+    assert set(source.iterate_keys()) == KEYS
+    target_root = get_store("s3://anystore")
+    assert target_root.get("test-mirror/lorem.txt") == target.get("lorem.txt")
+
+    mirrored, skipped = mirror(target, source)
+    assert mirrored == 0
+    assert skipped == 6
+    assert set(target.iterate_keys()) == KEYS
+    assert set(source.iterate_keys()) == KEYS
+
+    target = get_store("memory://")
+    mirrored, skipped = mirror(source, target)
+    assert mirrored == 6
+    assert skipped == 0
+    assert source.get("lorem.txt") == target.get("lorem.txt")
+    assert set(target.iterate_keys()) == KEYS
+    assert set(source.iterate_keys()) == KEYS
